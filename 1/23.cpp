65a36815d8fef3f5ca6bf1dfe1736bd5675e3c64
quantumblin <gogasarus@gmail.com>
2026-09-10 15:46:56 +0300

    Добавил clang-format файл для форматирования и сформатировал код

--- 1/23.cpp
+++ 1/23.cpp
@@ -1,19 +1,19 @@
 /*
 Условие: На вход подаются сведения о сдаче экзаменов учениками 9-х
-классов некоторой средней школы. В первой строке сообщается 
+классов некоторой средней школы. В первой строке сообщается
 количество учеников N, которое не меньше 10, но не превосходит
 100, каждая из следующих N строк имеет следующий формат:
-<Фамилия> <Имя> <оценки>, где <Фамилия> – строка, состоящая не 
+<Фамилия> <Имя> <оценки>, где <Фамилия> – строка, состоящая не
 более чем из 20 символов, <Имя> – строка, состоящая не более чем
-из 15 символов <оценки> – через пробел три целых числа, 
-соответствующие оценкам по пятибалльной системе. <Фамилия> и 
-<Имя>, а также <Имя> и <оценки> разделены одним пробелом. 
+из 15 символов <оценки> – через пробел три целых числа,
+соответствующие оценкам по пятибалльной системе. <Фамилия> и
+<Имя>, а также <Имя> и <оценки> разделены одним пробелом.
 Пример входной строки: Иванов Петр 4 5 4
-Требуется написать программу, которая будет выводить на экран 
-фамилии и имена трех лучших по среднему баллу учеников. Если 
-среди остальных есть ученики, набравшие тот же средний балл, 
-что и один из трех лучших, то следует вывести и их фамилии и 
-имена. Требуемые имена и фамилии можно выводить в произвольном 
+Требуется написать программу, которая будет выводить на экран
+фамилии и имена трех лучших по среднему баллу учеников. Если
+среди остальных есть ученики, набравшие тот же средний балл,
+что и один из трех лучших, то следует вывести и их фамилии и
+имена. Требуемые имена и фамилии можно выводить в произвольном
 порядке (8).
 
 Автор: Камалов Данил Маратович
@@ -31,89 +31,87 @@ using namespace std;
 
 struct Pupil
 {
-    string surname;
-    string name;
-    int marksSum;
+	string surname;
+	string name;
+	int marksSum;
 };
-
 
 void printTopPupils(const vector<Pupil>& pupils)
 {
-    if (pupils.empty()) return;
+	if (pupils.empty())
+		return;
 
-    int minScore = (pupils.size() >= 3) ? pupils[2].marksSum : pupils.back().marksSum;
+	int minScore = (pupils.size() >= 3) ? pupils[2].marksSum : pupils.back().marksSum;
 
-    for (const Pupil& pupil: pupils)
-    {
-        if (pupil.marksSum < minScore) break;
-        cout << pupil.surname << " " << pupil.name << "\n";
-    }
+	for (const Pupil& pupil : pupils)
+	{
+		if (pupil.marksSum < minScore)
+			break;
+		cout << pupil.surname << " " << pupil.name << "\n";
+	}
 }
 
 void sortPupils(vector<Pupil>& pupils)
 {
-    int n = pupils.size();
+	int n = pupils.size();
 
-    for (int i = 0; i < n - 1; ++i)
-    {
-        for (int j = 0; j < n - i - 1; ++j)
-        {
-            if (pupils[j].marksSum < pupils[j + 1].marksSum)
-            {
-                Pupil temp = pupils[j];
-                pupils[j] = pupils[j + 1];
-                pupils[j + 1] = temp;
-            }
-        }
-    }
+	for (int i = 0; i < n - 1; ++i)
+	{
+		for (int j = 0; j < n - i - 1; ++j)
+		{
+			if (pupils[j].marksSum < pupils[j + 1].marksSum)
+			{
+				Pupil temp = pupils[j];
+				pupils[j] = pupils[j + 1];
+				pupils[j + 1] = temp;
+			}
+		}
+	}
 }
 
 void readPupils(vector<Pupil>& pupils, int count)
 {
-    pupils.resize(count);
+	pupils.resize(count);
 
-    for (int i = 0; i < count; ++i)
-    {
-        int m1, m2, m3;
-        cin >> pupils[i].surname >> pupils[i].name >> m1 >> m2 >> m3;
-        pupils[i].marksSum = m1 + m2 + m3;
-    }
+	for (int i = 0; i < count; ++i)
+	{
+		int m1, m2, m3;
+		cin >> pupils[i].surname >> pupils[i].name >> m1 >> m2 >> m3;
+		pupils[i].marksSum = m1 + m2 + m3;
+	}
 }
-
-
 
 int main(int argc, char* argv[])
 {
-    if (argc < 3)
-    {
-        cerr << "Error: missing parametres!\n";
-        cerr << "Use: " << argv[0] << " <input file> <output file>\n";
-        return 1;
-    }
-    
-    if (freopen(argv[1], "r", stdin) == nullptr)
-    {
-        cerr << "Error: failed to open the file '" << argv[1] << "'!\n";
-        return 1;
-    }
+	if (argc < 3)
+	{
+		cerr << "Error: missing parametres!\n";
+		cerr << "Use: " << argv[0] << " <input file> <output file>\n";
+		return 1;
+	}
 
-    if (freopen(argv[2], "w", stdout) == nullptr)
-    {
-        cerr << "Error: failed to open/create the file '" << argv[2] << "'!\n";
-        return 1;
-    }
+	if (freopen(argv[1], "r", stdin) == nullptr)
+	{
+		cerr << "Error: failed to open the file '" << argv[1] << "'!\n";
+		return 1;
+	}
 
+	if (freopen(argv[2], "w", stdout) == nullptr)
+	{
+		cerr << "Error: failed to open/create the file '" << argv[2] << "'!\n";
+		return 1;
+	}
 
-    int count;
+	int count;
 
-    if (cin >> count)
-    {
-        vector<Pupil> pupils;
+	if (cin >> count)
+	{
+		vector<Pupil> pupils;
 
-        readPupils(pupils, count);
-        sortPupils(pupils);
-        printTopPupils(pupils);
-    }
+		readPupils(pupils, count);
+		sortPupils(pupils);
+		printTopPupils(pupils);
+	}
 
-    return 0;
+	return 0;
 }
